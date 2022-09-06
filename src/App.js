b926000bc7aba71fcfeb8ba713e2cf2956307973
Janet Leung <leung.janetw@hotmail.com>
2022-09-06 15:12:32 -0400

    Flexes header and changes colour

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import "./Header.scss";
 import logo from "../../assets/images/logos/InStock-Logo_2x.png"
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header className="header">
-            <img className="header__logo" src={logo} alt="instock" />
+            <Link to="/" className="header__logo-link"><img className="header__logo" src={logo} alt="instock" /></Link>
+            <div className="header__nav">
+                <span className="header__nav-item">Warehouses</span>
+                <span className="header__nav-item">Inventory</span>
+            </div>
         </header>
     );
 }
