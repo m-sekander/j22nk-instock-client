ab801d06382b08773230e5a995c34d778997fce3
Moin Sekander <moinuddin8510@gmail.com>
2022-09-06 14:57:14 -0400

    setup basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.scss';
+import {BrowserRouter, Routes, Route} from react-router-dom;
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/warehouses" element=""/>
+        <Route path="/warehouses/:warehouseId" element=""/>
+        <Route path="/warehouses/:warehouseId/edit" element=""/>
+        <Route path="/warehouses/add" element=""/>
+        <Route path="/inventories" element=""/>
+        <Route path="/inventories/:inventoryId" element=""/>
+        <Route path="/inventories/:inventoryId/edit" element=""/>
+        <Route path="/inventories/add" element=""/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
