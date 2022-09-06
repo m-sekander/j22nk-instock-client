5460e7073541ddf656bbedf7d24f36bc418cda31
Janet Leung <leung.janetw@hotmail.com>
2022-09-06 15:58:39 -0400

    Adds styles for desktop header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,12 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header className="header">
-            <Link to="/" className="header__logo-link"><img className="header__logo" src={logo} alt="instock" /></Link>
-            <div className="header__nav">
-                <span className="header__nav-item">Warehouses</span>
-                <span className="header__nav-item">Inventory</span>
+            <div className="header__content">
+                <Link to="/" className="header__logo-link"><img className="header__logo" src={logo} alt="instock" /></Link>
+                <div className="header__nav">
+                    <span className="header__nav-item">Warehouses</span>
+                    <span className="header__nav-item">Inventory</span>
+                </div>
             </div>
         </header>
     );
