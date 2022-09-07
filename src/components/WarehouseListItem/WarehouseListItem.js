780f3862f0ddbe8d62f07667a35c10e23e2244d6
Janet Leung <leung.janetw@hotmail.com>
2022-09-07 15:39:56 -0400

    Fixes styles of tablet by setting the order of the list items to the correct order (show contact as 3rd item)

--- src/components/WarehouseListItem/WarehouseListItem.js
+++ src/components/WarehouseListItem/WarehouseListItem.js
@@ -16,7 +16,7 @@ function WarehouseListItem({ id, name, address, city, country, contactName, cont
                     </p>
                 </Link>
             </div>
-            <div className="warehouse__container warehouse__name-container">
+            <div className="warehouse__container">
                 <h4 className="warehouse__label">Contact Name</h4>
                 <p className="warehouse__contact-name warehouse__info">{contactName}</p>
             </div>
