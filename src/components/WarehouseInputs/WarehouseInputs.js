ca9ce440f053c6b376081a35f4266321ccaa4fdf
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-08 06:54:23 -0400

    Set index as key in WarehouseInputs loop

--- src/components/WarehouseInputs/WarehouseInputs.js
+++ src/components/WarehouseInputs/WarehouseInputs.js
@@ -4,9 +4,9 @@ function WarehouseInputs({title, inputFields}) {
     return (
         <div className="warehouse-inputs">
             <h2>{title}</h2>
-            {inputFields.map((inputField) => {
+            {inputFields.map((inputField, index) => {
                 return (
-                    <label className="warehouse-inputs__label">
+                    <label key={index} className="warehouse-inputs__label">
                         {inputField}
                         <input className="warehouse-inputs__input" placeholder={inputField} name="warehouseName"></input>
                     </label>
