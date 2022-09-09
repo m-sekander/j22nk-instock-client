fe87b3e8f4fc98bf0106826fe9114331f27be846
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-08 23:22:29 -0400

    Fix to put initial values to defaultValues attribute

--- src/components/WarehouseInputs/WarehouseInputs.js
+++ src/components/WarehouseInputs/WarehouseInputs.js
@@ -1,7 +1,9 @@
 import "./WarehouseInputs.scss";
 import errorIcon from "../../assets/images/icons/error-24px.svg"
+import { useState } from "react";
 
 function WarehouseInputs({title, inputFields, fieldNames, errorMessages, fieldValues, className}) {
+
     return (
         <div className={"warehouse-inputs " + className}>
             <h2>{title}</h2>
@@ -13,7 +15,7 @@ function WarehouseInputs({title, inputFields, fieldNames, errorMessages, fieldVa
                             className="warehouse-inputs__input" 
                             placeholder={inputField} 
                             name={fieldNames[index]}
-                            value={fieldValues && fieldValues[index]}
+                            defaultValue={fieldValues[index]}
                             ></input>
                         {errorMessages[index] &&
                             <p className="warehouse-inputs__error">
