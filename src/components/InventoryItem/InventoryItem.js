ea648a2317904d2fc45a7b4fb8f30a3f79091180
Robert Loo <rloolee89@gmail.com>
2022-09-08 23:22:09 -0400

    J22NK-18: Add styling and table headers
    - add table headers for desktop and tablet
    - fix icon styling for mobile
    - fix warehouse label styling, by adding minimum width

--- src/components/InventoryItem/InventoryItem.js
+++ src/components/InventoryItem/InventoryItem.js
@@ -18,29 +18,42 @@ function InventoryItem({ item }) {
   return (
     <div className="inventory-list-main">
       <div className="inventory__list">
-        <div className="inventory-list__container-left">
+        {/* <div className="inventory-list__container-left"> */}
+        <div className="inventory-list__container">
           <h4 className="inventory-list__label">INVENTORY ITEM</h4>
           <div className="inventory-list__label-container">
             <p className="inventory-list__label-item">{item.itemName}</p>
-            <img className="inventory-list__label-icon" src={rightIcon} alt="" />
+            <img className="inventory-list__label-iconarrow" src={rightIcon} alt="" />
           </div>
+        </div>
+        <div className="inventory-list__container">
+          <h4 className="inventory-list__label">STATUS</h4>
+          <p className={getStatusClass()}>{item.status}</p>
+        </div>
+        <div className="inventory-list__container">
           <h4 className="inventory-list__label">CATEGORY</h4>
           <p className="inventory-list__label-category">{item.category}</p>
         </div>
-        <div className="inventory-list__container-right">
-          <h4 className="inventory-list__label">STATUS</h4>
-          <p className={getStatusClass()}>{item.status}</p>
-          <div className="inventory-list-qty">
-          <h4 className="inventory-list__label">QTY</h4>
-          <p className="inventory-list__label-qty">{item.quantity}</p>
+        {/* </div> */}
+        {/* <div className="inventory-list__container-right"> */}
+          <div className="inventory-list-qty inventory-list__container">
+            <h4 className="inventory-list__label">QTY</h4>
+            <p className="inventory-list__label-qty">{item.quantity}</p>
           </div>
-          <h4 className="inventory-list__label">WAREHOUSE</h4>
-          <p className="inventory-list__label-warehouse">{item.warehouseName}</p>
-        </div>
       </div>
-      <div style={{display: "flex" , justifyContent: "space-between"}}>
-        <img src={deleteIcon} alt="Delete icon" />
-        <Link to="/inventories/:inventoryId/edit"><img src={editIcon} alt="Edit icon" /></Link>
+        <div className="inventory-list__container-warehouse">
+          <div className="inventory-list__content-warehouse">
+            <h4 className="inventory-list__label">WAREHOUSE</h4>
+            <p className="inventory-list__label-warehouse">{item.warehouseName}</p>
+          </div>
+        </div>
+        {/* </div> */}
+      <div className="inventory-list__label-icon">
+        <h4 className="inventory-list__label inventory-list__action-label">ACTION</h4>
+        <div className="inventory-list__actions">
+          <img src={deleteIcon} alt="Delete icon" />
+          <Link to="/inventories/:inventoryId/edit"><img className="inventory-list__deleteicon" src={editIcon} alt="Edit icon" /></Link>
+        </div>
       </div>
     </div> 
   );
--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import InventoryItem from "../InventoryItem/InventoryItem";
 import Button from "../CTA/CTA";
 import { useEffect, useState } from "react";
+import sortIcon from "../../assets/images/icons/sort-24px.svg";
 
 function InventoryList() {
   const [inventoryList, setInventoryList] = useState([]);
@@ -21,14 +22,57 @@ function InventoryList() {
     <section className="inventory-list">
       <div className="inventory-list__header">
         <h1 className="inventory-list__title">Inventory</h1>
-        <form className="inventory-list__form"> 
-        <input className="inventory-list__search" type="text" placeholder="Search..." />
-        <div className="inventory-list__button"> 
-          <Button text={"+ Add New Item"} link="/inventories/add"/>
-        </div>
+        <form className="inventory-list__form">
+          <input
+            className="inventory-list__search"
+            type="text"
+            placeholder="Search..."
+          />
+          <div className="inventory-list__button">
+            <Button text={"+ Add New Item"} link="/inventories/add" />
+          </div>
         </form>
       </div>
-        {inventoryList.map((inventory)=> <InventoryItem key={inventory.id} item={inventory}/>)}
+      <div className="warehouse-list__label-strip">
+        <div className="warehouse-list__label-container">
+          <h4 className="warehouse-list__label-item">Warehouse</h4>
+          <img
+            className="warehouse-list__label-icon"
+            src={sortIcon}
+            alt="Sort icon"
+          />
+        </div>
+        <div className="warehouse-list__label-container">
+          <h4 className="warehouse-list__label-item">Address</h4>
+          <img
+            className="warehouse-list__label-icon"
+            src={sortIcon}
+            alt="Sort icon"
+          />
+        </div>
+        <div className="warehouse-list__label-container">
+          <h4 className="warehouse-list__label-item">Contact Name</h4>
+          <img
+            className="warehouse-list__label-icon"
+            src={sortIcon}
+            alt="Sort icon"
+          />
+        </div>
+        <div className="warehouse-list__label-container">
+          <h4 className="warehouse-list__label-item">Contact Information</h4>
+          <img
+            className="warehouse-list__label-icon"
+            src={sortIcon}
+            alt="Sort icon"
+          />
+        </div>
+        <div className="warehouse-list__label-container">
+          <h4 className="warehouse-list__label-item">Actions</h4>
+        </div>
+      </div>
+      {inventoryList.map((inventory) => (
+        <InventoryItem key={inventory.id} item={inventory} />
+      ))}
     </section>
   );
 }
