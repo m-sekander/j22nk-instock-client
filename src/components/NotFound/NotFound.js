7428b43bba78aeeb23b799ae0f825ec0a1b4092c
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-10 08:29:36 -0400

    Add link to homepage to NotFound

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import "./NotFound.scss";
+import CTA from "../CTA/CTA";
 
 function NotFound() {
     return (
         <section className="not-found">
             <h1 className="not-found__title">404 Not Found</h1>
-            <p className="not-found__description">Oops...looks like the page you're trying to access doesn't exist yet.</p>
+            <p className="not-found__description">Oops...we can't seem to find the page you're looking for.</p>
+            <CTA text="Go to homepage" link="/"/>
         </section>
     );
 }
