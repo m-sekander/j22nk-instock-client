00016909daa319038d03a15dece1c6d65116c4bc
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-10 08:15:55 -0400

    Apply styling to NotFound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,10 @@ import "./NotFound.scss";
 
 function NotFound() {
     return (
-        <h1 className="not-found">Not Found!</h1>
+        <section className="not-found">
+            <h1 className="not-found__title">404 Not Found</h1>
+            <p className="not-found__description">Oops...looks like the page you're trying to access doesn't exist yet.</p>
+        </section>
     );
 }
 
